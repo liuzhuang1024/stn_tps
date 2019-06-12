adea7e9ba298e175c4b525dd543413969726ff34
lz <18437961053@163.com>
2019-06-12 12:20:34 +0800

    once again

--- mnist_model_summuy.py
+++ mnist_model_summuy.py
@@ -16,21 +16,20 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(20, 1, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_output)
+        self.fc1 = nn.Linear(16, 160)
+        self.fc2 = nn.Linear(160, num_output)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv3(x)), 2))
+        x = x.view(-1, 16)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return x
-
-
-from mnist_model_summuy import CNN
 
 
 class ClsNet(nn.Module):
@@ -63,6 +62,8 @@ class BoundedGridLocNet(nn.Module):
 class UnBoundedGridLocNet(nn.Module):
 
     def __init__(self, grid_height, grid_width, target_control_points):
+        self.grid_height = grid_height
+        self.grid_width = grid_width
         super(UnBoundedGridLocNet, self).__init__()
         self.cnn = CNN(grid_height * grid_width * 2)
 
@@ -73,7 +74,7 @@ class UnBoundedGridLocNet(nn.Module):
     def forward(self, x):
         batch_size = x.size(0)
         points = self.cnn(x)
-        return points.view(batch_size, -1, 2)
+        return points.view(batch_size, self.grid_height * self.grid_width, 2)
 
 
 class STNClsNet(nn.Module):
@@ -82,12 +83,12 @@ class STNClsNet(nn.Module):
         super(STNClsNet, self).__init__()
         self.args = args
 
-        r1 = args.span_range_height
-        r2 = args.span_range_width
+        r1 = args['span_range_height']
+        r2 = args['span_range_width']
         assert r1 < 1 and r2 < 1  # if >= 1, arctanh will cause error in BoundedGridLocNet
         target_control_points = torch.Tensor(list(itertools.product(
-            np.arange(-r1, r1 + 0.00001, 2.0 * r1 / (args.grid_height - 1)),
-            np.arange(-r2, r2 + 0.00001, 2.0 * r2 / (args.grid_width - 1)),
+            np.arange(-r1, r1 + 0.00001, 2.0 * r1 / (args['grid_height'] - 1)),
+            np.arange(-r2, r2 + 0.00001, 2.0 * r2 / (args['grid_width'] - 1)),
         )))
         Y, X = target_control_points.split(1, dim=1)
         target_control_points = torch.cat([X, Y], dim=1)
@@ -95,28 +96,52 @@ class STNClsNet(nn.Module):
         GridLocNet = {
             'unbounded_stn': UnBoundedGridLocNet,
             'bounded_stn': BoundedGridLocNet,
-        }[args.model]
-        self.loc_net = GridLocNet(args.grid_height, args.grid_width, target_control_points)
+        }[args['model']]
+        self.loc_net = GridLocNet(args['grid_height'], args['grid_width'], target_control_points)
 
-        self.tps = TPSGridGen(args.image_height, args.image_width, target_control_points)
+        self.tps = TPSGridGen(args['image_height'], args['image_width'], target_control_points)
 
         self.cls_net = ClsNet()
 
     def forward(self, x):
         batch_size = x.size(0)
+        # loc has error
+        # TODO:right
         source_control_points = self.loc_net(x)
         source_coordinate = self.tps(source_control_points)
-        grid = source_coordinate.view(batch_size, self.args.image_height, self.args.image_width, 2)
+        grid = source_coordinate.view(batch_size, self.args['image_height'], self.args['image_width'], 2)
         transformed_x = grid_sample(x, grid)
         logit = self.cls_net(transformed_x)
         return logit
 
 
 def get_model(args):
-    if args.model == 'no_stn':
+    if args['model'] == 'no_stn':
         print('create model without STN')
         model = ClsNet()
     else:
         print('create model with STN')
         model = STNClsNet(args)
     return model
+
+
+if __name__ == '__main__':
+    arg = {
+        "model": "unbounded_stn",
+        "grid_height": 4,
+        "grid_width": 4,
+        "image_height": 64,
+        "image_width": 64,
+        "span_range_height": 0.9,
+        "span_range_width": 0.9,
+
+    }
+    model = get_model(arg)
+    cnn = CNN(10)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # PyTorch v0.4.0
+    cnn.to(device)
+    model.to(device)
+    from torchsummary import summary
+
+    summary(cnn, (1, 64, 64))
+    summary(model, (1, 64, 64))
